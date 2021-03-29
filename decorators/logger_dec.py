bc413eec6dbc267f53a478eb741500030cf12796
Peter Hoburg <peterHoburg@users.noreply.github.com>
2021-03-29 09:46:15 -0600

    #3: Added logging decorator.

---
+++ decorators/logger_dec.py
@@ -0,0 +1,20 @@
+import functools
+
+from config.log import log
+
+
+def pre_post_logger(outer_func=None, logging_level: str = "info"):
+    def _decorate(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            getattr(log, logging_level)("pre")
+            return_val = func(*args, **kwargs)
+            getattr(log, logging_level)("post")
+            return return_val
+
+        return wrapper
+
+    if outer_func is not None:
+        return _decorate(outer_func)
+
+    return _decorate
